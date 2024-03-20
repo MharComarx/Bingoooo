672cdc06bd82002dc6ca49f38fb76b44ca74ea09
Mharco Agbayani <agbayanimharco0@gmail.com>
2024-03-20 11:50:25 +0800

    backtick

---
+++ index.js
@@ -0,0 +1,76 @@
+console.log("Holla World");
+let isB = false, isI = false, isN = false, isG = false, isO = false;
+
+let BNumber, INumber, NNumber, GNumber, ONumber;
+
+
+
+//new number
+
+function generateNumber(maxNumber) {
+
+
+    // Addition = "+"
+    // Subtraction = "-"
+    // Multiplication = "*"
+    // Division = "/"
+    // Remainder = "%"
+
+    let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+
+    // if ... else if ... else statement
+    // Conditional operator
+    // GE >=, LTE <=, EQ ==, NEQ ! =, AND &&, OR ||
+    if (randomNumber <= 15) {
+        BNumber = randomNumber;
+        isB = true;
+        console.log(`The Number ${randomNumber} is in B`);
+
+    } else if (randomNumber >= 16 && randomNumber <= 30) {
+        INumber = randomNumber;
+        isI = true;
+        console.log(`The Number ${randomNumber} is in I`);
+
+    } else if (randomNumber >= 31 && randomNumber <= 45) {
+        NNumber = randomNumber;
+        isN = true;
+        console.log(`The Number is ${randomNumber} in N`);
+
+    } else if (randomNumber >= 46 && randomNumber <= 60) {
+        GNumber = randomNumber;
+        isG = true;
+        console.log(`The Number is ${randomNumber} in G`);
+
+    } else if (randomNumber >= 61 && randomNumber <= 75) {
+        ONumber = randomNumber;
+        isO = true;
+        console.log(`The Number ${randomNumber} is inO`);
+
+    } else {
+        console.log('The number is invalid.');
+    }
+
+    if (isB == true && isI == true && isN == true && isG == true && isO == true) {
+        console.log('BINGOOO');
+        console.table({
+            B: BNumber,
+            I: INumber,
+            N: NNumber,
+            G: GNumber,
+            O: ONumber,
+        })
+    }
+
+}
+
+
+let counter = 10;
+
+while (
+    isB == false ||
+    isI == false ||
+    isN == false ||
+    isG == false ||
+    isO == false) {
+    generateNumber(75)
+}
